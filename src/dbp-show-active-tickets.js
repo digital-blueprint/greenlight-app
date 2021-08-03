be96efdf868315203f9fd4e02b752c72b4f6e8f6
Christina Tögl <toegl@tugraz.at>
2021-08-03 15:53:15 +0200

    Add example content and layout for show active tickets activity

--- src/dbp-show-active-tickets.js
+++ src/dbp-show-active-tickets.js
@@ -15,6 +15,9 @@ class ShowActiveTickets extends ScopedElementsMixin(DBPGreenlightLitElement) {
         this._i18n = createInstance();
         this.lang = this._i18n.language;
         this.entryPointUrl = '';
+        this.activeTickets = [];
+        this.loading = false;
+        this._initialFetchDone = false;
     }
 
     static get scopedElements() {
@@ -31,6 +34,9 @@ class ShowActiveTickets extends ScopedElementsMixin(DBPGreenlightLitElement) {
             ...super.properties,
             lang: { type: String },
             entryPointUrl: { type: String, attribute: 'entry-point-url' },
+            activeTickets: { type: Array, attribute: false },
+            initialTicketsLoading: { type: Boolean, attribute: false },
+            loading: { type: Boolean, attribute: false },
         };
     }
 
@@ -51,6 +57,43 @@ class ShowActiveTickets extends ScopedElementsMixin(DBPGreenlightLitElement) {
         super.update(changedProperties);
     }
 
+    parseActiveTickets(response) {
+        let list = [];
+
+        //TODO
+        list[0] = { location: { name: 'Test Location' }, endTime: new Date() };
+
+        return list;
+    }
+
+    async getActiveTickets() {
+        let response = { };
+
+        //TODO
+        response.status = 200;
+
+        return response;
+    }
+
+    /**
+     * Get a list of active checkins
+     *
+     * @returns {Array} list
+     */
+    async getListOfActiveTickets() {
+        this.initialTicketsLoading = !this._initialFetchDone;
+        try {
+            let response = await this.getActiveTickets();
+            let responseBody = await response; //await response.json(); //TODO
+            if (responseBody !== undefined && responseBody.status !== 403) {
+                this.activeTickets = this.parseActiveTickets(responseBody);
+            }
+        } finally {
+            this.initialTicketsLoading = false;
+            this._initialFetchDone = true;
+        }
+    }
+
     static get styles() {
         // language=css
         return css`
@@ -59,11 +102,67 @@ class ShowActiveTickets extends ScopedElementsMixin(DBPGreenlightLitElement) {
             ${commonStyles.getNotificationCSS()}
             ${CheckinStyles.getCheckinCss()}
             ${commonStyles.getButtonCSS()}
+            
+            .tickets {
+                display: flex;
+                justify-content: space-between;
+                column-gap: 15px;
+                row-gap: 1.5em;
+                align-items: center;
+                margin-top: 2em;
+            }
+
+            .btn {
+                display: flex;
+                justify-content: space-between;
+                column-gap: 0.5em;
+            }
+
+            .header {
+                display: grid;
+                align-items: center;
+            }
+            
+            .border {
+                margin-top: 2rem;
+                padding-top: 2rem;
+                border-top: 1px solid black;
+            }
+
+            @media only screen
+            and (orientation: portrait)
+            and (max-width:768px) {
+
+                .tickets {
+                    display: block;
+                }
+
+                .header {
+                    margin-bottom: 0.5rem;
+                }
+
+                #delete-btn {
+                    margin-bottom: 2rem;
+                }
+
+                .btn {
+                    flex-direction: column;
+                    row-gap: 0.5em;
+                }
+                
+                .loading{
+                    justify-content: center;
+                }
+            }
         `;
     }
 
     render() {
         const i18n = this._i18n;
+
+        if (this.isLoggedIn() && !this.isLoading() && !this._initialFetchDone && !this.initialTicketsLoading) {
+            this.getListOfActiveTickets();
+        }
 
         return html`
 
@@ -80,12 +179,30 @@ class ShowActiveTickets extends ScopedElementsMixin(DBPGreenlightLitElement) {
             <div class="${classMap({hidden: !this.isLoggedIn() || this.isLoading()})}">
                 
                 <h2>${i18n.t('show-active-tickets.title')}</h2>
-                <div>
-                    <p class="">${i18n.t('show-active-tickets.description')}</p>
-                    <slot name="additional-information">
-                        <p>${i18n.t('show-active-tickets.additional-information')}</p>
-                    </slot>
+                <p>${i18n.t('show-active-tickets.description')}</p>
+                
+                <div class="border tickets ${classMap({hidden: !this.isLoggedIn() || this.isLoading()})}">
+                    ${ this.activeTickets.map(i => html`
+
+                        <span class="header">
+                            <strong>${i.location.name}</strong>
+                            ${this.getReadableDate(i.endTime)}
+                        </span>
+                        <div class="btn">
+                            <dbp-loading-button type="is-primary" ?disabled="${this.loading}" value="${i18n.t('show-active-tickets.show-btn-text')}" @click="${(event) => {}}" title="${i18n.t('show-active-tickets.show-btn-text')}"></dbp-loading-button>
+                            <dbp-loading-button id="refresh-btn" ?disabled="${this.loading}" value="${i18n.t('show-active-tickets.refresh-btn-text')}" @click="${(event) => {}}" title="${i18n.t('show-active-tickets.refresh-btn-text')}"></dbp-loading-button>
+                            <dbp-loading-button id="delete-btn" ?disabled="${this.loading}" value="${i18n.t('show-active-tickets.delete-btn-text')}" @click="${(event) => {}}" title="${i18n.t('show-active-tickets.delete-btn-text')}"></dbp-loading-button>
+                        </div>
+                    `)}
+                    <span class="control ${classMap({hidden: this.isLoggedIn() && !this.initialTicketsLoading})}">
+                        <span class="loading">
+                            <dbp-mini-spinner text=${i18n.t('loading-message')}></dbp-mini-spinner>
+                        </span>
+                    </span>
+                    
+                    <div class="no-tickets ${classMap({hidden: !this.isLoggedIn() || this.initialTicketsLoading || this.activeTickets.length !== 0})}">${i18n.t('show-active-tickets.no-tickets-message')}</div>
                 </div>
+
             </div>
         `;
     }
