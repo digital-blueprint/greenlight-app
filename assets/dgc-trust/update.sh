ea6750229fc708a5c8ec23a213fa4cbf622acdea
Christoph Reiter <reiter.christoph@gmail.com>
2021-08-30 15:57:49 +0200

    Move the trust files into a separate subdirectory
    this way we can share the subdirectory between deployments

--- assets/dgc-trust/update.sh
+++ assets/dgc-trust/update.sh
@@ -3,12 +3,14 @@
 set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
+PROD_DIR="${DIR}/prod"
 DGC_TRUST_URL="https://dgc-trust.qr.gv.at"
 
-curl --silent --fail "${DGC_TRUST_URL}/trustlist" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/trustlistsig"
-curl --silent --fail "${DGC_TRUST_URL}/trustlistsig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/trustlistsig"
-curl --silent --fail "${DGC_TRUST_URL}/rules" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/rules"
-curl --silent --fail "${DGC_TRUST_URL}/rulessig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/rulessig"
-curl --silent --fail "${DGC_TRUST_URL}/valuesets" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/valuesets"
-curl --silent --fail "${DGC_TRUST_URL}/valuesetssig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/valuesetssig"
+
+mkdir -p "${PROD_DIR}"
+curl --silent --fail "${DGC_TRUST_URL}/trustlist" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${PROD_DIR}/trustlistsig"
+curl --silent --fail "${DGC_TRUST_URL}/trustlistsig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${PROD_DIR}/trustlistsig"
+curl --silent --fail "${DGC_TRUST_URL}/rules" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${PROD_DIR}/rules"
+curl --silent --fail "${DGC_TRUST_URL}/rulessig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${PROD_DIR}/rulessig"
+curl --silent --fail "${DGC_TRUST_URL}/valuesets" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${PROD_DIR}/valuesets"
+curl --silent --fail "${DGC_TRUST_URL}/valuesetssig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${PROD_DIR}/valuesetssig"
