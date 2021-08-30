04883aba781e848fcf82f3d683c78751f93e97f6
Christoph Reiter <reiter.christoph@gmail.com>
2021-08-30 15:36:17 +0200

    Write files relative to the script; pass --silent; make sure to not replace files on error
    It's not clear from the curl docs if it will not touch the output if it fails,
    so write to a temporary file just to be sure.

--- assets/dgc-trust/update.sh
+++ assets/dgc-trust/update.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 DGC_TRUST_URL="https://dgc-trust.qr.gv.at"
 
-curl --fail "${DGC_TRUST_URL}/trustlist" -o ./trustlist
-curl --fail "${DGC_TRUST_URL}/trustlistsig" -o ./trustlistsig
-curl --fail "${DGC_TRUST_URL}/rules" -o ./rules
-curl --fail "${DGC_TRUST_URL}/rulessig" -o ./rulessig
-curl --fail "${DGC_TRUST_URL}/valuesets" -o ./valuesets
-curl --fail "${DGC_TRUST_URL}/valuesetssig" -o ./valuesetssig
+curl --silent --fail "${DGC_TRUST_URL}/trustlist" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/trustlistsig"
+curl --silent --fail "${DGC_TRUST_URL}/trustlistsig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/trustlistsig"
+curl --silent --fail "${DGC_TRUST_URL}/rules" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/rules"
+curl --silent --fail "${DGC_TRUST_URL}/rulessig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/rulessig"
+curl --silent --fail "${DGC_TRUST_URL}/valuesets" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/valuesets"
+curl --silent --fail "${DGC_TRUST_URL}/valuesetssig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${DIR}/valuesetssig"
