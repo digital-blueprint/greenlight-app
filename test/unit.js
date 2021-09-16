9d3063c73cd83231f110f28aca4acb2bb4be1e09
Christoph Reiter <reiter.christoph@gmail.com>
2021-09-16 14:59:24 +0200

    Remove unused code

--- test/unit.js
+++ test/unit.js
@@ -1,7 +1,6 @@
 import {assert} from 'chai';
 
 import '../src/dbp-greenlight.js';
-import {parseQRCode} from '../src/utils.js';
 
 suite('dbp-greenlight-app basics', () => {
   let node;
@@ -21,23 +20,3 @@ suite('dbp-greenlight-app basics', () => {
   });
 });
 
-suite('parseQRCode', () => {
-    test('ok', () => {
-        assert.deepEqual(parseQRCode('foo: -loc-1', 'foo'), ['loc', 1]);
-        assert.deepEqual(parseQRCode("bla foo \n foo: -loc-1", 'foo'), ['loc', 1]);
-        assert.deepEqual(parseQRCode("bla foo \n foo: -loc-0", 'foo'), ['loc', 0]);
-        assert.deepEqual(parseQRCode("bla foo \n foo: -loc-42 ", 'foo'), ['loc', 42]);
-        assert.deepEqual(parseQRCode("bla foo \n foo: -loc-", 'foo'), ['loc', null]);
-        assert.deepEqual(parseQRCode("bla foo \n foo: -loc", 'foo'), ['loc', null]);
-    });
-
-    test('not ok', () => {
-        assert.throws(() => parseQRCode('asdad', 'foo'));
-        assert.throws(() => parseQRCode('foo:', 'foo'));
-        assert.throws(() => parseQRCode('foo: -', 'foo'));
-        assert.throws(() => parseQRCode('foo: -loc-bla', 'foo'));
-        assert.throws(() => parseQRCode('foo: --', 'foo'));
-        assert.throws(() => parseQRCode('foo: -loc-1-', 'foo'));
-        assert.throws(() => parseQRCode('foo: -loc-1-3', 'foo'));
-    });
-});
