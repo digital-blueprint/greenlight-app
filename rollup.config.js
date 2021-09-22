b1edde1935547cff1ff434b05c3224f10ac902a6
Tamara Steinwender <tamara.steinwender@tugraz.at>
2021-09-22 15:27:55 +0200

    Add greenlight icon in different resolutions

--- rollup.config.js
+++ rollup.config.js
@@ -206,7 +206,7 @@ export default (async () => {
                     {src: 'assets/datenschutzerklaerung-tu-graz-greenlight.pdf', dest: 'dist/' + await getDistPath(pkg.name)},
                     {src: 'assets/htaccess-shared', dest: 'dist/shared/', rename: '.htaccess'},
                     {src: 'assets/icon-*.png', dest: 'dist/' + await getDistPath(pkg.name)},
-                    {src: 'assets/apple-*.png', dest: 'dist/' + await getDistPath(pkg.name)},
+                    {src: 'assets/icon/*', dest: 'dist/' + await getDistPath(pkg.name, 'icon')},
                     {src: 'assets/wbstudkart.jpeg', dest: 'dist/' + await getDistPath(pkg.name)},
                     {src: 'assets/images/*', dest: 'dist/images'},
                     {src: 'assets/manifest.json', dest: 'dist', rename: pkg.internalName + '.manifest.json'},
