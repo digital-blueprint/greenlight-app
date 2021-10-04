465019391b89e8532782483a98e083294f5d51ef
Christoph Reiter <reiter.christoph@gmail.com>
2021-10-04 16:53:27 +0200

    Fix a typo

--- src/hcert/utils.js
+++ src/hcert/utils.js
@@ -54,7 +54,7 @@ export async function importHCert() {
     // we inject it into the body, extract it from window
     // and then try to cover all traces of what we did.
     // So we can kinda pretend that this is like an ES module..
-    let promise = importHCert._pomise;
+    let promise = importHCert._promise;
     if (promise === undefined) {
         promise = new Promise((resolve, reject) => {
             let script = document.createElement('script');
@@ -71,7 +71,7 @@ export async function importHCert() {
             script.src = commonUtils.getAssetURL(pkgName, 'hcert-kotlin.js');
             document.head.appendChild(script);
           });
-          importHCert._pomise = promise;
+          importHCert._promise = promise;
     }
 
     return withFakeBigInt(async () => {return await promise;});
