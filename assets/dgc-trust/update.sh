3bf6c466e4bc4d4e024e912ab01b6cacac38f20c
Christoph Reiter <reiter.christoph@gmail.com>
2021-11-07 19:10:02 +0100

    cron script: download the rules in json format as well from our new mirror

--- assets/dgc-trust/update.sh
+++ assets/dgc-trust/update.sh
@@ -5,9 +5,11 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 TRUST_DIR="${DIR}/prod"
 DGC_TRUST_URL="https://dgc-trust.qr.gv.at"
-
+DCC_RULES_URL="https://digital-blueprint.github.io/dcc-at-rule-sets/sets"
+RULE_SET="${1:-TUGRAZ}"
 
 mkdir -p "${TRUST_DIR}"
+curl --silent --fail "${DCC_RULES_URL}/${RULE_SET}.json" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${TRUST_DIR}/rules.json"
 curl --silent --fail "${DGC_TRUST_URL}/trustlist" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${TRUST_DIR}/trustlist"
 curl --silent --fail "${DGC_TRUST_URL}/trustlistsig" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${TRUST_DIR}/trustlistsig"
 curl --silent --fail "${DGC_TRUST_URL}/rules" -o "${DIR}/_temp" && mv "${DIR}/_temp" "${TRUST_DIR}/rules"
