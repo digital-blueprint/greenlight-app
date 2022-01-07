4cd3f5120bf8e2a017cf73b2c4b2f5399f66812c
Christoph Reiter <reiter.christoph@gmail.com>
2022-01-07 17:48:49 +0100

    Register dbp-info-tooltip globally and use it in the template overrides

--- src/dbp-greenlight.js
+++ src/dbp-greenlight.js
@@ -2,7 +2,9 @@ import {AppShell} from '@dbp-toolkit/app-shell';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import {Translated} from "@dbp-toolkit/common/src/translated";
 import {TUGrazLogo} from "@dbp-toolkit/app-shell/src/tugraz-logo";
+import {InfoTooltip} from '@dbp-toolkit/tooltip';
 
 commonUtils.defineCustomElement('dbp-greenlight', AppShell);
 commonUtils.defineCustomElement('dbp-translated', Translated);
 commonUtils.defineCustomElement('dbp-tugraz-logo', TUGrazLogo);
+commonUtils.defineCustomElement('dbp-info-tooltip', InfoTooltip);
