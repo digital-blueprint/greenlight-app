fb3bceca00ebb278619cac2834536fd2d348a5f1
Christoph Reiter <reiter.christoph@gmail.com>
2022-07-28 16:50:36 +0200

    Move to @tugraz packages for tugraz specific code
    We want to default to white label in the long run, this is just
    a starting point so we can remove the code from the toolkit

--- src/dbp-greenlight.js
+++ src/dbp-greenlight.js
@@ -2,10 +2,10 @@ import '@webcomponents/scoped-custom-element-registry';
 import {AppShell} from '@dbp-toolkit/app-shell';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import {Translated} from '@dbp-toolkit/common/src/translated';
-import {TUGrazLogo} from '@dbp-toolkit/app-shell/src/tugraz-logo';
+import {Logo} from '@tugraz/web-components';
 import {Icon} from '@dbp-toolkit/common';
 
 commonUtils.defineCustomElement('dbp-greenlight', AppShell);
 commonUtils.defineCustomElement('dbp-translated', Translated);
-commonUtils.defineCustomElement('dbp-tugraz-logo', TUGrazLogo);
 commonUtils.defineCustomElement('dbp-icon', Icon);
+commonUtils.defineCustomElement('tug-logo', Logo);
